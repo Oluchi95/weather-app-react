e75134aaade44b24323b47fe0a4f18e18ec7ebd4
Oluchi95 <ikeripriscilla94@gmail.com>
2024-04-24 15:23:31 +0100

    made some changes

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a City..."
-              className="form-control"
-              autoFocus="on"
-            ></input>
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    if (response.data.main) {
+      console.log(response.data);
+      setTemperature(response.data.main.temp);
+      setReady(true);
+    } else {
+      console.error("Unable to get temperature data from response:", response);
+    }
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a City..."
+                className="form-control"
+                autoFocus="on"
+              ></input>
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              ></input>
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            ></input>
-          </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Monday 19:00</li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
+        </form>
+        <h1>New York</h1>
+        <ul>
+          <li>Monday 19:00</li>
+          <li>Sunny</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
             <img
               src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
               alt="Sunny"
             ></img>
-            <span className="temperature">13</span>
+            <span className="temperature">{temperature}</span>
             <span className="unit">°C | °F</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 0%</li>
-            <li>Humidity: 43%</li>
-            <li>Wind: 14 km/h</li>
-          </ul>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 0%</li>
+              <li>Humidity: 43%</li>
+              <li>Wind: 14 km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
